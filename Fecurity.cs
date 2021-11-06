a2e9b93c09ae2d7cb2ba1600ecca4a901aceca3a
iSquare Kent <arturgaffarov33@gmail.com>
2021-11-06 13:55:26 +0300

    Добавьте файлы проекта.

---
+++ Fecurity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Fecurity
+{
+    public unsafe static class Entry
+    {
+        [DllExport(CallingConvention = CallingConvention.StdCall)]
+        public static int FMain(int SyncHook)
+        {
+            return SyncHook;
+        }
+    }
+}
+
