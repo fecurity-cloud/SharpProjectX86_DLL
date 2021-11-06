0600f35eb120dae771baeadc1cc3ac66b0338fcb
iSquare Kent <arturgaffarov33@gmail.com>
2021-11-06 14:05:56 +0300

    DLLExport

--- Fecurity.cs
+++ Fecurity.cs
@@ -5,10 +5,16 @@ namespace Fecurity
 {
     public unsafe static class Entry
     {
+        [DllImport("User32.dll", EntryPoint = "MessageBox", CharSet = CharSet.Auto)]
+        internal static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
         [DllExport(CallingConvention = CallingConvention.StdCall)]
-        public static int FMain(int SyncHook)
+        public static int DllMain(void* hModule, int dwReason, IntPtr lpReserved)
         {
-            return SyncHook;
+            return MessageBox(
+                IntPtr.Zero, 
+                String.Format("dwReason: {0}\nlpReserved: {1:X}", dwReason, lpReserved), 
+                "Message", 0);
         }
     }
 }
